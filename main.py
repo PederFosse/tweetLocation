67f7fe7ae84ac21255b8cdae35ffd1cd1ec06066
PederFosse <peder.fosse@gmail.com>
2021-05-10 15:52:18 +0200

    finished project

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 new_york_tweets = pd.read_json('new_york.json', lines=True)
 london_tweets = pd.read_json('london.json', lines=True)
@@ -15,4 +20,21 @@ all_tweets = new_york_text + london_text + paris_text
 labels = [0] * len(new_york_text) + [1] * len(london_text) + [2] * len(paris_text)
 
 # divide set into train and test set
+train_data, test_data, train_labels, test_labels = train_test_split(all_tweets, labels, test_size=0.2, random_state=1)
 
+# create a counter and transform train/test data
+counter = CountVectorizer()
+counter.fit(train_data + test_data)
+train_counts = counter.transform(train_data)
+test_counts = counter.transform(test_data)
+
+# create a NB classifier, fit it with the training data and create predictions on the test data to evaluate the model
+classifier = MultinomialNB()
+classifier.fit(train_counts, train_labels)
+predictions = classifier.predict(test_counts)
+
+# classify by using accuracy_score
+# print(accuracy_score(test_labels, predictions))
+
+# classify by using confusion matrix
+# print(confusion_matrix(test_labels, predictions))
